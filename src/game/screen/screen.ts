50a58d29d73c6c074e63af9f5650e8886111f928
Serega <sergeykyz19998@gmail.com>
2023-12-27 22:20:02 +0200

    just added a timer

--- src/game/screen/screen.ts
+++ src/game/screen/screen.ts
@@ -1,11 +1,13 @@
 import { Config } from "../config/game";
 import { Face } from "./face";
 import { Field } from "./field";
+import { Timer } from "./timer";
 
 type ClickEventHandler = (e: Event) => void;
 
 export class Screen {
   private _face = new Face();
+  private _timer = new Timer();
   private _field: Field;
 
   constructor(config: Config, leftClickHandler: ClickEventHandler, rightClickHandler: ClickEventHandler) {
@@ -16,6 +18,7 @@ export class Screen {
 
   public init(): void {
     this._face.init();
+    this._timer.init();
     this._field.init();
   }
 }
--- src/game/screen/timer.ts
+++ src/game/screen/timer.ts
@@ -1,3 +1,56 @@
-export class Timer {
+import { GameEvent, GameEventType, gameObserver } from "../observable/game";
+// import { FaceView } from "./view/face";
 
+export class Timer {
+  private _element: HTMLSpanElement = document.querySelector(
+    ".miner__timer span"
+  ) as HTMLSpanElement;
+  private _startTime = Date.now();
+  private _currentTime: string = "000";
+  private _loopTimer: any;
+
+  constructor() {
+    gameObserver.attach(this.observerHandler.bind(this));
+  }
+
+  private observerHandler(data: GameEventType) {
+    if (data === GameEvent.start) {
+      this.start();
+    }
+
+    if (data === GameEvent.win || data === GameEvent.lose) {
+      this.stop();
+    }
+  }
+
+  public init(): void {
+    this.drawTime();
+    this.start();
+  }
+
+  private start() {
+    this._startTime = Date.now();
+    this._loopTimer = setInterval(() => {
+      this.calcTime();
+      this.drawTime();
+    }, 1000);
+  }
+
+  private stop() {
+    clearInterval(this._loopTimer);
+  }
+
+  private calcTime() {
+    const currentTime = Math.floor((Date.now() - this._startTime) / 1000);
+
+    const seconds = currentTime % 60;
+    const minutes = Math.floor((currentTime - seconds) / 60);
+    const subSecond = seconds < 10 ? "0" : "";
+
+    this._currentTime = `${minutes}${subSecond}${seconds}`;
+  }
+
+  private drawTime() {
+    this._element.textContent = this._currentTime;
+  }
 }
