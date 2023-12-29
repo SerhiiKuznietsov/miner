4dd3c8e1fac6c93e794108ee5f661c3c0e015daf
Serega <sergeykyz19998@gmail.com>
2023-12-29 10:59:07 +0200

    fixed code and removed unused file

--- src/game/stateControllers/states/spawners/index.ts
+++ src/game/stateControllers/states/spawners/index.ts
@@ -1,11 +1,9 @@
-import { ActionName, ActionNamesList } from "../../../actions/actions";
-import { TailStateController } from "../../tailStateController";
-import { StateList, StateNameType, StateNamesList } from "../type/type";
+import { StateList } from "../type/type";
 import { State } from "../state";
 import { StateController } from "../../stateController";
 
 // { name: , actionToState: }
-interface IStateControllerOptions<N, A> {
+export interface IStateControllerOptions<N, A> {
   defaultState: N;
   statesOptionsList: Array<IStateOptions<N, A>>;
 }
@@ -24,7 +22,7 @@ const spawnState = <N, A>(
   return state;
 };
 
-const spawnStateController = <N, A>(
+export const spawnStateController = <N, A>(
   options: IStateControllerOptions<N, A>
 ): StateController<N, A> => {
   const { defaultState, statesOptionsList } = options;
@@ -42,102 +40,3 @@ const spawnStateController = <N, A>(
   return stateController;
 };
 
-const spawnTailStateController = spawnStateController<
-  StateNameType,
-  ActionName
->;
-
-export const spawnEmptyTailState = (): TailStateController => {
-  return spawnTailStateController({
-    defaultState: StateNamesList.closeState,
-    statesOptionsList: [
-      {
-        name: StateNamesList.closeState,
-        actionToState: [
-          [ActionNamesList.rightClick, StateNamesList.flagState],
-          [ActionNamesList.leftClick, StateNamesList.emptyState],
-          [ActionNamesList.calc, StateNamesList.emptyState],
-        ],
-      },
-      {
-        name: StateNamesList.flagState,
-        actionToState: [
-          [ActionNamesList.rightClick, StateNamesList.closeState],
-          [ActionNamesList.calc, StateNamesList.emptyState],
-          [ActionNamesList.lose, StateNamesList.falseFlagState],
-        ],
-      },
-      {
-        name: StateNamesList.falseFlagState,
-        actionToState: [],
-      },
-      {
-        name: StateNamesList.emptyState,
-        actionToState: [],
-      },
-    ],
-  });
-};
-
-export const spawnAroundTailState = (): TailStateController => {
-  return spawnTailStateController({
-    defaultState: StateNamesList.closeState,
-    statesOptionsList: [
-      {
-        name: StateNamesList.closeState,
-        actionToState: [
-          [ActionNamesList.rightClick, StateNamesList.flagState],
-          [ActionNamesList.leftClick, StateNamesList.aroundState],
-          [ActionNamesList.calc, StateNamesList.aroundState],
-        ],
-      },
-      {
-        name: StateNamesList.flagState,
-        actionToState: [
-          [ActionNamesList.rightClick, StateNamesList.closeState],
-          [ActionNamesList.calc, StateNamesList.aroundState],
-          [ActionNamesList.lose, StateNamesList.falseFlagState],
-        ],
-      },
-      {
-        name: StateNamesList.falseFlagState,
-        actionToState: [],
-      },
-      {
-        name: StateNamesList.aroundState,
-        actionToState: [],
-      },
-    ],
-  });
-};
-
-export const spawnMineTailState = (): TailStateController => {
-  return spawnTailStateController({
-    defaultState: StateNamesList.closeState,
-    statesOptionsList: [
-      {
-        name: StateNamesList.closeState,
-        actionToState: [
-          [ActionNamesList.rightClick, StateNamesList.flagState],
-          [ActionNamesList.leftClick, StateNamesList.redMineState],
-          [ActionNamesList.win, StateNamesList.flagState],
-          [ActionNamesList.lose, StateNamesList.mineState],
-        ],
-      },
-      {
-        name: StateNamesList.flagState,
-        actionToState: [
-          [ActionNamesList.rightClick, StateNamesList.closeState],
-        ],
-      },
-      {
-        name: StateNamesList.redMineState,
-        actionToState: [],
-      },
-      {
-        name: StateNamesList.mineState,
-        actionToState: [],
-      },
-    ],
-  });
-};
