9e0a241f4f009c3c4898d836bb33b8326dd536e9
Serega <sergeykyz19998@gmail.com>
2024-02-25 03:33:10 +0200

    logic for displaying cells has been moved

--- src/game/logic/field/cellController.ts
+++ src/game/logic/field/cellController.ts
@@ -1,35 +1,9 @@
 import { Config } from "../../config/game";
 import { TailDataType, tailStateObservable } from "../../observable/tailState";
-import {
-  StateNameType,
-  StateNamesList,
-} from "../../stateControllers/states/type/type";
 import { createId } from "../../utils/id";
 import { Cell } from "./cell";
+import { getAttrValueByState } from "./utils/cellView";
 import { createFieldCell } from "./utils/createCell";
-
-const stateToView: Record<StateNameType, Function> = {
-  [StateNamesList.aroundState]: (around: number = 0): string => `type${around}`,
-  [StateNamesList.closeState]: () => "closed",
-  [StateNamesList.emptyState]: () => "type0",
-  [StateNamesList.falseFlagState]: () => "mine_wrong",
-  [StateNamesList.flagState]: () => "flag",
-  [StateNamesList.mineState]: () => "mine",
-  [StateNamesList.redMineState]: () => "mine_red",
-};
-
-const getAttrValueByState = (
-  stateName: StateNameType,
-  around: number | undefined
-): string => {
-  if (!stateToView.hasOwnProperty(stateName)) {
-    throw new Error(`state with name "${stateName}" not found`);
-  }
-
-  const func: Function = stateToView[stateName];
-
-  return func(around);
-};
 
 export class CellController {
   private _cellsList = new Map<string, Cell>();
---
+++ src/game/logic/field/utils/cellView.ts
@@ -0,0 +1,27 @@
+import {
+  StateNameType,
+  StateNamesList,
+} from "../../../stateControllers/states/type/type";
+
+const stateToView: Record<StateNameType, Function> = {
+  [StateNamesList.aroundState]: (around: number = 0): string => `type${around}`,
+  [StateNamesList.closeState]: () => "closed",
+  [StateNamesList.emptyState]: () => "type0",
+  [StateNamesList.falseFlagState]: () => "mine_wrong",
+  [StateNamesList.flagState]: () => "flag",
+  [StateNamesList.mineState]: () => "mine",
+  [StateNamesList.redMineState]: () => "mine_red",
+};
+
+export const getAttrValueByState = (
+  stateName: StateNameType,
+  around: number | undefined
+): string => {
+  if (!stateToView.hasOwnProperty(stateName)) {
+    throw new Error(`state with name "${stateName}" not found`);
+  }
+
+  const func: Function = stateToView[stateName];
+
+  return func(around);
+};
